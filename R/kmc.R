bdab4bf6b21dd70fbe9aed1dff23f230dcb2b78c
Yifan <yfyang.86@gmail.com>
2014-11-13 14:59:33 -0500

    KMC.R
    Add a bjtest function in 2 dimension.

--- R/kmc.R
+++ R/kmc.R
@@ -29,6 +29,9 @@ kmc.clean <- function(kmc.time,delta){
  return (list(kmc.time=kmc.time,delta=delta));
 }
 
+
+
+
 omega.lambda<-cmpfun(function(kmc.time,delta,lambda,g,gt.mat){
   #iter
   p=length(g);# the number of constraint
@@ -230,6 +233,68 @@ kmc.solve<-function(x,d,g,em.boost=T,using.num=T,using.Fortran=T,using.C=F,tmp.t
     class(re.tmp)<-"kmcS3";
 	return(re.tmp);
 }
+
+kmc.bjtest<-function(
+  y, d, x, beta
+){
+  
+  n <- length(y)
+  x <- as.matrix(x)
+  xdim <- dim(x)
+  if (xdim[1] != n) 
+    stop("check dim of x")
+  if (length(beta) != xdim[2]) 
+    stop("check dim of x and beta")
+  e <- y - as.vector(x %*% beta)
+  ordere <- order(e, -d)
+  esort <- e[ordere]
+  dsort <- d[ordere]
+  xsort <- as.matrix(x[ordere, ])
+  dsort[length(dsort)] <- 1
+  temp0 <- WKM(esort, dsort, zc = 1:n)
+  pKM <- temp0$jump
+  temp <- redistF(y = esort, d = dsort, Fdist = pKM)
+  weight <- temp$weight/n
+  A <- matrix(0, ncol = xdim[2], nrow = n)
+  for (i in 1:n) if (dsort[i] == 1) {
+    A[i, ] <- t(as.matrix(weight[1:i, i])) %*% xsort[1:i,]
+    A[i, ] <- A[i, ]/pKM[i]
+  }
+  
+  
+  gt.matrix = esort*A;
+  
+  kmc.comb123<-function(x){
+    kmc_routine4(lambda=x,delta=delta,gtmat=gt.matrix)->re;
+    return(re);
+  }
+  
+  u.lambda2<-function(re=el.cen.EM2.kmc(x=kmc.time,d=delta,fun=function(x){cbind(g[[1]](x),g[[2]](x))},mu=c(0,0),maxit=5,debug.kmc=F)){
+    del.loc=which(delta==1)[1:2];
+    tmp=c(0,0);
+    if (del.loc[2]!=2) tmp[2]=sum(as.numeric(delta[1:(del.loc[2]-1)]==0)/( rep(1-re$prob[1],2) ) )
+    UD<-cbind(g[[1]](re$times[1:2]),g[[2]](re$times[1:2]))
+    uu.lambda=as.vector(
+      solve(UD)%*%(n-1/re$prob[del.loc]-tmp)
+    )
+    # debug oupur lambda: print(uu.lambda)
+    uu.lambda
+  }
+  init.lam=u.lambda2()
+  multiroot(kmc.comb123,start=init.lam,useFortran = T)$root -> lambda
+  
+  omega.lambda(kmc.time=esort,delta=delta,lambda=lambda,g=NULL,gt.mat=gt.matrix) -> result; ## set lambda=0, it compute KM-est
+  
+  temp2<-kmc.el(delta,result$omega,result$S)
+  
+  
+  pnew <- result$omega
+  logel1 <- temp0$logel
+  logel2 <- temp2
+  list(prob = pnew, logel = logel1, logel2 = logel2, `-2LLR` = 2 * 
+         (logel1 - logel2))
+}
+
 
 plotkmc2D <-function(resultkmc,flist=list(f1=function(x){x},f2=function(x){x^2}),range0=c(0.2,3,20)){
 	tmp.df=length(resultkmc$g);
